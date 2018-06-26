a2997ea9eaacd677269066d29615e1e88a0ff56b
unknown <adam9312@comcast.net>
2018-06-25 19:50:36 -0700

    Av

--- server/routes/sendbottle.js
+++ server/routes/sendbottle.js
@@ -68,6 +68,19 @@ router.post('/messages', function(req, res) {
   .save(function(err, message) {
     console.log(message)
   });
+  // Get the count of all users
+  sendbottle.count().exec(function (err, count) {
+
+  // Get a random entry
+  var random = Math.floor(Math.random() * count)
+
+  // Again query all users but only fetch one offset by our random #
+  sendbottle.findOne().skip(random).exec(
+    function (err, result) {
+      // Tada! random user
+      console.log(result) 
+    })
+})
 });
 
 module.exports = router;
