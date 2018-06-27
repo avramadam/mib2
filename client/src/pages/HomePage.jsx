f12e7063c6ee6432171d7cf673f87119e05bda9a
Bri <bherreras23@gmail.com>
2018-06-26 18:06:22 -0700

    Tinkering

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,25 +8,28 @@ import { Card } from 'material-ui/Card';
 
 const Dashboard = () => (
   <Card className="container">
- <div>
-    <Hero backgroundImage="./images/miab.jpeg">
-      <h1>Message in a Bottle</h1>
-      <h2>What message might make your day?</h2>
-      <div className="test">
+    <div>
+      <Hero backgroundImage="./images/miab.jpeg">
+        <h1>Message in a Bottle</h1>
+        <h2>What message might make your day?</h2>
+        <div className="test">
           <p>
-         We encourage communication by reaching out and sending a message without an intended recipient.  Share your thoughts, feeling, moods, links, suggestions, complaints, worries, fears, secrets, etc.  
-         
-            Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed malesuada
-            molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi ac mi
-            hendrerit semper. Nulla tincidunt accumsan lobortis.
-      
-            Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-            tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh sit
-            amet odio lobortis ullamcorper. 
+            Messages in bottles have been used to send distress messages; in scientific studies;
+            as memorial tributes; to convey expedition reports, and to carry letters or reports from those believing
+            themselves to be doomed.  Messages in bottles have been used as invitations to prospective pen pals and letters
+          to actual or imagined love interests have also been sent as messages in bottles. </p>
+          <p>
+            Our app is a modern take on some of those same concepts. The lore surrounding messages in bottles has often
+            been of a romantic or poetic nature. And finding a bottle has normally been viewed as a positive
+            experience. We hope to spread some of that excitement and positivity here.
           </p>
-      </div>
-    </Hero>
-  </div>
+          <p>
+            We encourage communication by reaching out and sending a message without an intended recipient.
+            Share your thoughts, feeling, moods, links, suggestions, complaints, worries, fears, secrets, etc.
+          </p>
+        </div>
+      </Hero>
+    </div>
   </Card>
 );
 
--- client/src/pages/SendBottleForm.jsx
+++ client/src/pages/SendBottleForm.jsx
@@ -12,49 +12,37 @@ const SendBottleForm = ({
   errors,
   user,
 }) => (
-  <Card className="container">
-    <form action="/" onSubmit={onSubmit}>
-      <h2 className="card-heading">Send Message</h2>
+    <Card className="container">
+      <form action="/" onSubmit={onSubmit}>
+        <h2 className="card-heading">Send Message</h2>
 
-      {errors.summary && <p className="error-message">{errors.summary}</p>}
+        {errors.summary && <p className="error-message">{errors.summary}</p>}
 
-      <div className="field-line">
-        <TextField
-          floatingLabelText="Name"
-          name="name"
-          errorText={errors.name}
-          onChange={onChange}
-          value={user.name}
-        />
-      </div>
+        <div className="field-line">
+          <TextField
+            floatingLabelText="Title"
+            name="Title"
+            onChange={onChange}
+            value={user.title}
+          />
+        </div>
 
-      <div className="field-line">
-        <TextField
-          floatingLabelText="Email"
-          name="email"
-          errorText={errors.email}
-          onChange={onChange}
-          value={user.email}
-        />
-      </div>
+        <div className="textarea">
+          <TextField
+            floatingLabelText="Message"
+            name="message"
+            onChange={onChange}
+            value={user.message}
+          />
+        </div>
 
-      <div className="field-line">
-        <TextField
-          floatingLabelText="Message"
-          name="message"
-          onChange={onChange}
-          //errorText={errors.message}
-          value={user.message}
-        />
-      </div>
+        <div className="button-line">
+          <RaisedButton type="submit" label="Send New Message" primary />
+        </div>
 
-      <div className="button-line">
-        <RaisedButton type="submit" label="Send New Message" primary />
-      </div>
-
-    </form>
-  </Card>
-);
+      </form>
+    </Card>
+  );
 
 SendBottleForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
@@ -64,8 +52,7 @@ SendBottleForm.propTypes = {
 };
 
 SendBottleForm.defaultProps = {
-  name: '',
-  email: '',
+  title: '',
   message: '',
 };
 
