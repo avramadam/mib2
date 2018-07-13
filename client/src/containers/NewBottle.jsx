f0c9dcfcdfd7113212f927e728105050b05d9d1a
unknown <adam9312@comcast.net>
2018-07-12 17:48:26 -0700

    final

--- client/src/containers/NewBottle.jsx
+++ client/src/containers/NewBottle.jsx
@@ -1,12 +1,4 @@
 import React from 'react';
-import Auth from '../modules/Auth';
-import Saved from '../pages/SavedPage.jsx';
-import PropTypes from 'prop-types';
-import { Button, Row, Col } from 'reactstrap'
-
-import LoginForm from '../pages/LoginForm.jsx';
-import { Card, CardTitle, CardText } from 'material-ui/Card';
-var store = require('store')
 import New from '../pages/NewBottlePage.jsx';
 
 
@@ -15,9 +7,6 @@ class NewBottles extends React.Component {
 
 	constructor(props, context) {
 		super(props, context);
-		//this.getMessages = this.getMessages.bind(this);
-		//test2
-		// set the initial component state
 		this.state = {
 			errors: {}
 		}
